2f3c42a4f611f2cf21436e082f4aebb2869158fc
unknown <2hojune@naver.com>
2023-06-07 18:50:16 +0900

    ✨ Feat: 문제 추가

---
+++ src/Baek/Baek_11725.java
@@ -0,0 +1,11 @@
+package Baek;
+
+import java.util.List;
+
+public class Baek_11725 {
+	static int N;
+	static List<int[]>[] graph;
+	public static void main(String[] args) {
+		graph = new List[N];
+	}
+}
