d71faddfde227efcbc0ff30fb52733c663cac096
unknown <2hojune@naver.com>
2023-06-09 15:14:26 +0900

    ✨ Feat: 문제 추가

--- src/Baek/Baek_11725.java
+++ src/Baek/Baek_11725.java
@@ -1,11 +1,67 @@
 package Baek;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Baek_11725 {
 	static int N;
-	static List<int[]>[] graph;
-	public static void main(String[] args) {
-		graph = new List[N];
+	static boolean[] isVisited;
+	static List<Integer>[] graph;
+	static int[] parent;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		graph = new ArrayList[N + 1];
+		isVisited = new boolean[N + 1];
+		parent = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
+		
+		for (int i = 0; i < N - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(b);
+			graph[b].add(a);
+		}
+		bfs();
+		for (int i = 2; i <= N; i++) {
+			sb.append(parent[i] + "\n");
+		}
+		System.out.println(sb);
+	}
+	static void bfs() {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(1);
+		isVisited[1] = true;
+		
+		while(!q.isEmpty()) {
+			int size = q.size();
+			for (int s = 0; s < size; s++) {
+				int cur = q.poll();
+				
+				for (int i = 0; i < graph[cur].size(); i++) {
+					int next = graph[cur].get(i);
+					if(!isVisited[next]) {
+						q.offer(next);
+						isVisited[next] = true;
+						parent[next] = cur;
+					}
+				}
+			}
+		}
 	}
 }
---
+++ src/Baek/Baek_2573.java
@@ -0,0 +1,12 @@
+package Baek;
+
+public class Baek_2573 {
+    static int N, M;
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        System.out.println(N + " " + M);
+    }
+}
