9b00aaff85b4986b273333906fd22c3602466c86
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-06-16 22:05:44 +0900

    [Gold IV] Title: 트리의 지름, Time: 232 ms, Memory: 22352 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
-
+/**
+ * 1. 루트 노드 (1번 노드)로부터 가장 먼 리프 노드에서 DFS
+ * 2. 리프 노드 (간선이 1개인 끝 노드) 집합에서 DFS
+ * */
 public class Main {
     static class node {
         int V, cost;
@@ -16,7 +19,7 @@ public class Main {
         }
 
     }
-    static int n, max = 0;
+    static int n, max = 0, furtherNode = 1;
     static boolean[] isVisited;
     static List<node>[] graph;
     public static void main(String[] args) throws IOException {
@@ -38,21 +41,23 @@ public class Main {
             graph[child].add(new node(parent, cost));
             graph[parent].add(new node(child, cost));
         }
-        for (int i = 1; i <= n; i++) {
-            isVisited[i] = true;
-            for (int j = 0; j < graph[i].size(); j++) {
-                node cur = graph[i].get(j);
-                isVisited[cur.V] = true;
-                dfs(cur.V, cur.cost);
-                isVisited[cur.V] = false;
-            }
-            isVisited[i] = false;
-        }
+
+        // 루트노드로부터 가장 멀리있는 리프노드를 구한다
+        isVisited[1] = true;
+        dfs(1, 0);
+        isVisited[1] = false;
+        max = 0;
+
+        // 리프노드로부터 가장 멀리있는 노드를 구함
+        isVisited[furtherNode] = true;
+        dfs(furtherNode, 0);
+
         System.out.println(max);
     }
     static void dfs(int cur, int sum) {
         if(sum > max) {
             max = sum;
+            furtherNode = cur;
         }
         for (int i = 0; i < graph[cur].size(); i++) {
             node next = graph[cur].get(i);
