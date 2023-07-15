1d8e41af3b8095229fc63116478faeb01a584b7a
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-07-15 22:17:43 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,52 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 파이프 옮기기 1
+ * */
 public class Main {
-	static int N, cnt = 0;
-	static int[][] graph;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		graph = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		dfs2(1, 0, 1);
-		System.out.println(cnt);
-	}
-	
-	static void dfs2(int type, int nx, int ny) {
-		if(nx == N && ny == N)
-			cnt++;
-		if(type==1) {
-			if(check(new int[] {nx, ny}) && graph[nx][ny]==0) {
-				dfs2(1, nx, ny+1);
-				dfs2(3, nx + 1, ny + 1);
-			}
-		} else if(type ==2) {
-			if(check(new int[] {nx, ny}) && graph[nx][ny]==0) {
-				dfs2(2, nx + 1, ny);
-				dfs2(3, nx + 1, ny + 1);
-			}
-		} else if(type == 3) {
-			if(check(new int[] {nx, ny}) && graph[nx][ny]==0 && graph[nx-1][ny]==0 && graph[nx][ny-1]==0) {
-				dfs2(1, nx, ny+1);
-				dfs2(2, nx + 1, ny);
-				dfs2(3, nx + 1, ny + 1);
-			}
-		}
-	}
-	static boolean check(int[] cur) {
-		if(cur[0]>=0 && cur[0]<N && cur[1]>=0 && cur[1]<N && graph[cur[0]][cur[1]]==0)
-			return true;
-		return false;
-	}
+    static int N, cnt = 0;
+    static int[][] graph, dir = {{0, 1}, {1, 0}, {1, 1}};
+    static int[][][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N + 1][N + 1];
+        dp = new int[N + 1][N + 1][3]; // 0:가로  1:세로  2:대각선
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[1][2][0] = 1;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 3; j <= N; j++) {
+                if(graph[i][j] == 0) { // 가로
+                    dp[i][j][0] += (dp[i][j-1][0] + dp[i][j-1][2]);
+                }
+                if(i - 1 >= 1 && graph[i][j] == 0) { // 세로
+                    dp[i][j][1] += (dp[i-1][j][1] + dp[i-1][j][2]);
+                }
+                if(i -1 >= 1 && graph[i][j] == 0 && graph[i-1][j] == 0 && graph[i][j-1] == 0) { // 대각선
+                    dp[i][j][2] += (dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2]);
+                }
+            }
+        }
+        System.out.println(dp[N][N][0] + dp[N][N][1] + dp[N][N][2]);
+    }
 }
