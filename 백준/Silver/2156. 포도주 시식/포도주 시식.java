17e93bf1915fa32663624b049d97d3a0b02983e0
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-07-20 14:38:53 +0900

    [Silver I] Title: 포도주 시식, Time: 152 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,26 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
+/**
+ * 포도주 시식
+ * 1부터 n까지의 포도주를 마실 때, 연속으로 놓인 3잔을 모두 마실 수 없음
+ * 최대 마실 수 있는 포도주 양
+ * dp : Bottom-Up 방식 (반복문 구현)
+ * */
 public class Main {
     static int n;
-    static int[] arr;
-    static int[][] dp;
+    static int[] arr, d;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n + 1];
-        dp = new int[n + 1][3]; // 0: 선택x  1: 선택o
+        d = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][2]));
-            dp[i][1] = dp[i-1][0] + arr[i];
-            dp[i][2] = dp[i-1][1] + arr[i];
+
+        d[1] = arr[1];
+        if(n > 1) {
+            d[2] = arr[1] + arr[2];
         }
-        System.out.println(Math.max(dp[n][0], Math.max(dp[n][1], dp[n][2])));
+        for (int i = 3; i <= n; i++) {
+            d[i] = Math.max(d[i-1], Math.max(d[i-2] + arr[i], d[i-3] + arr[i-1] + arr[i]));
+        }
+        System.out.println(d[n]);
     }
 }
