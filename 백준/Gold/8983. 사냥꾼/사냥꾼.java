03870fd5905f45b5c1420ce3461a36f91d2cc24f
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-09-25 21:23:51 +0900

    [Gold IV] Title: 사냥꾼, Time: 720 ms, Memory: 57832 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.java
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.java
@@ -1,60 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-    static int M, N, L; // 사대의 수, 동물의 수, 사정거리
-    static int[] loc;
-    static List<int[]> animals;
+ 
+    static int M, N, L, range[];
+    static Animal[] animals;
+ 
     public static void main(String[] args) throws IOException {
+ 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-
-        loc = new int[M];
-        animals = new ArrayList<>();
-
+ 
+        M = Integer.parseInt(st.nextToken()); // 사대의 수
+        N = Integer.parseInt(st.nextToken()); // 동물의 수
+        L = Integer.parseInt(st.nextToken()); // 사정거리
+        range = new int[M];
+        animals = new Animal[N];
+ 
+        // 사대 정보
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            loc[i] = Integer.parseInt(st.nextToken());
+            range[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(loc);
-
+ 
+        // 동물의 위치 정보
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            animals.add(new int[]{x, y});
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+ 
+            animals[i] = new Animal(a, b);
         }
-
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            int[] cur = animals.get(i);
-            int lo = 0, hi = M, mid = -1;
-            while(lo <= hi) {
-                mid = (lo + hi)/2;
-
-                if(mid >= M) {
-                    break;
-                }
-                int l = cur[1] + Math.abs(cur[0] - loc[mid]);
-                if(L >= l) {
-                    cnt += 1;
-                    break;
-                } else if(loc[mid] >= cur[0]) {
-                    hi -= 1;
-                } else {
-                    lo += 1;
-                }
-            }
-        }
-        System.out.println(cnt);
+ 
+        System.out.println(process());
     }
+ 
+    private static int process() {
+ 
+        int res = 0;
+        // 사대의 위치를 정렬
+        Arrays.sort(range);
+ 
+        // 동물들은 자신을 잡을 수 있는 가까운 사대를 찾는다.
+        for (int i = 0; i < N; i++) {
+            res += search(i);
+        }
+ 
+        return res;
+    }
+ 
+    // 이분 탐색
+    private static int search(int idx) {
+        
+        int start = 0, end = M, mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if(mid >= M) return 0;
+            
+            int dist = getDist(animals[idx].r, animals[idx].c, range[mid]);
+            // 사정거리가 오른쪽 밖에 있을 경우
+            if (L < dist && animals[idx].r < range[mid]) end = mid - 1;
+            // 사정거리가 왼쪽 밖에 있을 경우
+            else if (L < dist && animals[idx].r >= range[mid]) start = mid + 1;
+            // 사정거리 안에 들어올 경우
+            else if (L >= dist) return 1;
+        }
+        
+        return 0;
+    }
+ 
+    private static int getDist(int a, int b, int x) {
+        return Math.abs(x - a) + b;
+    }
+ 
+    static class Animal {
+        int r, c;
+ 
+        public Animal(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+ 
 }
