d76440381dcddec4d8ab42be3615d8aa1840463e
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-09-25 18:11:26 +0900

    [Gold IV] Title: 사냥꾼, Time: 128 ms, Memory: 14236 KB, Score: 9 point -BaekjoonHub

---
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int M, N, L; // 사대의 수, 동물의 수, 사정거리
+    static int[] loc;
+    static List<int[]> animals;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        loc = new int[M];
+        animals = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            loc[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(loc);
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            animals.add(new int[]{x, y});
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            int[] cur = animals.get(i);
+            int lo = 0, hi = M-1, mid = -1;
+            while(lo <= hi) {
+                mid = (lo + hi)/2;
+                if(loc[mid] >= cur[0]) {
+                    hi -= 1;
+                } else {
+                    lo += 1;
+                }
+            }
+            if(mid != -1 && cur[1] + Math.abs(cur[0] - loc[mid]) <= L) {
+                cnt += 1;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
