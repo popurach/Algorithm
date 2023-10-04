ce642f039e450bb2d79d8bd91e92d72b7a015b10
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-10-04 19:08:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 192 ms, Memory: 53800 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,30 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static int[] W, V, d;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        d = new int[K + 1];
-        W = new int[N + 1];
-        V = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
-        }
-        for (int i = 1; i <= N; i++) {
-            for (int j = K; j - W[i] >= 0; j--) {
-                d[j] = Math.max(d[j], d[j - W[i]] + V[i]);
-            }
-        }
-        System.out.println(d[K]);
-    }
+	static int N, K;//물품의 수, 버틸 수 있는 무게
+	static int[][] ob;
+	static int[][] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		ob = new int[N + 1][2];
+		dp = new int[N + 1][K + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			ob[i][0] = w;
+			ob[i][1] = v;
+		}
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= K; j++) {
+				if(j - ob[i][0] >= 0) {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - ob[i][0]] + ob[i][1]);
+				}else {
+					dp[i][j] = dp[i-1][j];
+				}
+			}
+		}
+//		for (int i = 1; i <= N; i++) {
+//			System.out.println(Arrays.toString(dp[i]));
+//		}
+		
+		System.out.println(dp[N][K]);
+	}
 }
