f7f64948aae885707820738da7b1569b7d82d2d8
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-10-14 22:39:13 +0900

    [Gold IV] Title: N-Queen, Time: 4404 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,35 +4,35 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int N, cnt = 0;
-    static int[] arr;
+    static boolean[] isChecked;
+    static int[] nqueen;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        arr = new int[N];
-        dfs(0);
+        nqueen = new int[N + 1];
+        dfs(1);
         System.out.println(cnt);
     }
     static void dfs(int depth) {
-        if(depth == N) {
+        if(depth == N + 1) {
             cnt += 1;
             return;
         }
-        for (int i = 0; i < N; i++) {
-            arr[depth] = i;
-            if(possible(depth)) {
+        for (int i = 1; i <= N; i++) {
+            if(check(depth, i)) {
+                nqueen[depth] = i;
                 dfs(depth + 1);
             }
         }
     }
-    static boolean possible(int col) {
-        for (int i = 0; i < col; i++) {
-            // 이전 행에 같은 값이 있는지 확인
-            if(arr[i] == arr[col]) {
+    static boolean check(int depth, int idx) {
+        // 세로, 대각선 방향
+        for (int i = 1; i < depth; i++) {
+            if(nqueen[i] == idx) {
                 return false;
             }
-            // 대각선 확인
-            else if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+            if(nqueen[i] + (depth - i) == idx || nqueen[i] - (depth - i) == idx) {
                 return false;
             }
         }
