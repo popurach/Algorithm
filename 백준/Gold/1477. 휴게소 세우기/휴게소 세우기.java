dac08f32b7eda24a3baaee37597e2e8af3d33e0b
HoJune Lee <51961392+popurach@users.noreply.github.com>
2023-10-20 18:10:46 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+public class Main {
+    static int N, M, L, lo = 1, hi = 1, answer;
+    static List<Integer> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        list.add(0);
+        list.add(L);
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        list.sort((a, b) -> a-b);
+
+        for (int i = 1; i < list.size(); i++) {
+            hi = Integer.max(hi, list.get(i) - list.get(i-1));
+        }
+        answer = L;
+        while(lo <= hi) {
+            int mid = (lo + hi)/2;
+
+            int cnt = 0;
+            for (int i = 1; i < list.size(); i++) {
+                cnt += (list.get(i) - list.get(i-1) - 1)/mid;
+            }
+
+            if(cnt > M) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        System.out.println(lo);
+    }
+}
